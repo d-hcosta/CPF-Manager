e40dd379792ec9576c9577231b60ad8e64beea27
Vrag <88698720+Vrag404@users.noreply.github.com>
2022-04-11 14:10:28 -0300

    Update cpf_validator.py

--- cpf_validator.py
+++ cpf_validator.py
@@ -1,12 +1,10 @@
 while True:
-    # Variables
     cpf = input('Enter a CPF:')
     format_cpf = cpf.replace(".", "").replace("-", "")
     new_cpf = format_cpf[:-2]
     reverse = 10
     total = 0
 
-    # Handling exceptions.
     try:
         if len(format_cpf) != 11:
             print('11 digits are required.')
@@ -17,7 +15,6 @@ while True:
     except Exception as e:
         print ('11 digits are required.')
 
-    # Algorithm.
     for index in range(19):
         if index > 8:
             index -= 9
@@ -34,7 +31,6 @@ while True:
             total = 0
             new_cpf += str(digit)
     
-    # Last check.
     sequence = new_cpf == str(new_cpf[0]) * len(format_cpf)
 
     if format_cpf == new_cpf and not sequence:
